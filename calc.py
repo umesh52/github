78aacdb7c98f6cbce34be84ddc77c46e678d23bd
umesh52 <umeshpatel6900@gmail.com>
2019-09-14 08:21:12 +0000

    1.0

---
+++ calc.py
@@ -0,0 +1,15 @@
+def add(x,y):
+        pass
+    def subtract(x,y):
+            pass
+        def multiply(x,y):
+                pass
+            def divide(x,y):
+                    pass
+                def square(x,y):
+                        pass
+                    git add -A
+                    make change in the calc.py
+                    def multiply(x,y):
+                            return x*y
+x
